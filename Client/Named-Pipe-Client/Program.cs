749ea5d820e5b970158c57956312f8e3c456f0e7
Win32k <11098753-win32k@users.noreply.gitlab.com>
2022-03-13 18:48:51 +0000

    first commit

---
+++ Client/Named-Pipe-Client/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.IO.Pipes;
+using System.Threading.Tasks;
+
+namespace Named_Pipe_Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task.Delay(1000).Wait();
+            var client = new NamedPipeClientStream("Pipe1"); // Create new client and connect to Pipe1
+            client.Connect();
+            StreamReader reader = new StreamReader(client);
+            StreamWriter writer = new StreamWriter(client);
+            Console.WriteLine("[+] Connected to named pipe");
+
+            while (true)
+            {
+                Console.Write("> ");
+                string input = Console.ReadLine();
+                if (string.IsNullOrEmpty(input)) break; // If input is null, break
+                writer.WriteLine(input);
+                writer.Flush();
+                Console.WriteLine(reader.ReadLine());
+            }
+        }
+    }
+}
---
+++ Server/Named-Pipe-Server/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.IO.Pipes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Named_Pipe_Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServePipe();
+            Task.Delay(1000).Wait();
+            Console.WriteLine("[*] Serving Named Pipe");
+            Console.ReadLine();
+        }
+
+
+        static void ServePipe()
+        {
+            Task.Factory.StartNew(() =>
+            {
+                var server = new NamedPipeServerStream("Pipe1");
+                server.WaitForConnection();
+                StreamReader reader = new StreamReader(server);
+                StreamWriter writer = new StreamWriter(server);
+                while (true)
+                {
+                    var line = reader.ReadLine();
+                    writer.WriteLine(String.Join("", line.Reverse()));
+                    writer.Flush();
+                }
+            });
+        }
+    }
+}
